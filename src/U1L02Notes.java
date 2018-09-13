08c8108889240debb37b173969b55fd78a305796
gronea <gronea@LR064W7M-1718.ellis.int>
2018-09-13 09:19:45 -0400

    This is the change making code that goes with the pseudo code(U1L02Notes)

--- src/U1L02Notes.java
+++ src/U1L02Notes.java
@@ -12,7 +12,30 @@ public class U1L02Notes {
 	 * 	remainder == pennies
 	 * 	print "For" + change + "in dollars equals" + nickel number + "in nickels and " + pennies
 	 * 		+ "in pennies."
+	 * OR
 	 */
+	 //Define variables (nickels, etc.)
+	 int nickels = 0;
+	 int pennies = 0;
+	 
+	 //Get input and Change into an integer
+	 String answer = JOptionPane.showInputDialog("Entere the monetary value (ex. 1.56): ");
+	 double cash = Double.parseDouble(answer); 
+			 // "double" variable for decimal
+	 //pennies = money * 100
+	 pennies = (int)(cash * 100);
+	 System.out.println(pennies);
+	 
+	 //While money > .05 {
+	 while (pennies > 5) {
+		//nickels += 1
+		 nickels++;
+		//money -= .05
+		 pennies -= 5;
+	 }
+	 
+	 //Print answer
+	 System.out.println(null, cash + "is" + nickels + "nickels and " + pennies + "pennies.");
 
 	}
 }
