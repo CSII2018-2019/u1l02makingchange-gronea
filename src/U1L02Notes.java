20b503415f8e0cb631dfdacd9368d06625176394
gronea <gronea@LR064W7M-1718.ellis.int>
2018-09-13 08:21:35 -0400

    This is U1L02 Pseudo code for calculating nickels and pennies from a number.

---
+++ src/U1L02Notes.java
@@ -0,0 +1,18 @@
+import javax.swing.JOptionPane;
+
+public class U1L02Notes {
+
+	public static void main(String[] args) {
+	/*
+	 * Pseudo code:
+	 * 	asks for the input of the cash amount
+	 * 	makes it an integer to use as a number
+	 * 	divide by 5 to get number of nickels (rounding to the lower whole #)
+	 * 	take change and subtract # times 5 to get the remainder
+	 * 	remainder == pennies
+	 * 	print "For" + change + "in dollars equals" + nickel number + "in nickels and " + pennies
+	 * 		+ "in pennies."
+	 */
+
+	}
+}
