69393e1d416cc4cf95bf6628a126cddda42c4041
gronea <gronea@LR064W7M-1718.ellis.int>
2018-09-13 09:31:59 -0400

    Mathematical method - pennies are not working ???

--- src/U1L02Notes.java
+++ src/U1L02Notes.java
@@ -3,17 +3,21 @@ import javax.swing.JOptionPane;
 public class U1L02Notes {
 
 	public static void main(String[] args) {
-	/*
-	 * Pseudo code:
-	 * 	asks for the input of the cash amount
-	 * 	makes it an integer to use as a number
-	 * 	divide by 5 to get number of nickels (rounding to the lower whole #)
-	 * 	take change and subtract # times 5 to get the remainder
-	 * 	remainder == pennies
-	 * 	print "For" + change + "in dollars equals" + nickel number + "in nickels and " + pennies
-	 * 		+ "in pennies."
-	 * OR
-	 */
+	
+	 //Pseudo code:
+	//asks for the input of the cash amount
+	String answer = JOptionPane.showInputDialog("Entere the monetary value (ex. 1.56): ");
+//	 makes it an integer to use as a number
+	double cash = Double.parseDouble(answer);
+//	 divide by 5 to get number of nickels (rounding to the lower whole #)
+	int nickels = (int)(cash/0.05);
+//	 take change and subtract # times 5 to get the remainder, which * 100 = pennies
+	int pennies = (int)((cash - .05 * nickels)) *100;
+//	 print "For" + change + "in dollars equals" + nickel number + "in nickels and " + pennies
+//	 + "in pennies."
+	JOptionPane.showMessageDialog(null, cash + " is " + nickels + " nickels and " + pennies + " pennies.");
+	 //OR
+	 /*
 	 //Define variables (nickels, etc.)
 	 int nickels = 0;
 	 int pennies = 0;
@@ -35,6 +39,7 @@ public class U1L02Notes {
 	 }
 	 
 	 //Print answer
-	 System.out.println(null, cash + "is" + nickels + "nickels and " + pennies + "pennies.");
+	 JOptionPane.showMessageDialog(null, cash + "is" + nickels + "nickels and " + pennies + "pennies.");
+	  */
 	}
 }
